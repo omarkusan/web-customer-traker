62ff93ff81d29abc7d0c687508d027564af42093
Omar Mehmet <omarkusan@gmail.com>
2017-04-16 18:15:41 -0500

    @PostMapping and @GetMapping

---
+++ src/com/omar/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.omar.service;
+
+import java.util.List;
+
+import com.omar.entity.Customer;
+
+public interface CustomerService {
+	public List<Customer> getCustomers();
+}
---
+++ src/com/omar/service/CustomerServiceImpl.java
@@ -0,0 +1,14 @@
+package com.omar.service;
+
+import java.util.List;
+
+import com.omar.entity.Customer;
+
+public class CustomerServiceImpl implements CustomerService{
+
+	@Override
+	public List<Customer> getCustomers() {
+		return null;
+	}
+
+}
