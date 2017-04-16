1325382ee043b0d0f861b627ba6bce70dae7eb7f
Omar Mehmet <omarkusan@gmail.com>
2017-04-16 11:51:50 -0500

    Adding Entity Class

--- src/com/omar/entity/Customer.java
+++ src/com/omar/entity/Customer.java
@@ -27,4 +27,42 @@ public class Customer {
 	
 	public Customer() {
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFistName() {
+		return fistName;
+	}
+
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", fistName=" + fistName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
 }
